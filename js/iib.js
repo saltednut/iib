f81b9e11ccfe188d82f493a44731e8e81ff2f9cb
brantwynn <brant.wynn@acquia.com>
2014-03-14 14:08:58 -0500

    Add Navbar and update Toolbar integrations

--- js/iib.js
+++ js/iib.js
@@ -1,19 +1,35 @@
 (function ($) {
     Drupal.behaviors.iib = {
       attach : function (context, settings) {
-        $('#toolbar #iib-toolbar-link', context).click(function() {
-          if ($(this).hasClass('active')) {
-            // Toggle to hidden/inactive
-            $(this).removeClass('active');
-            $('.page-iib', context).addClass('iib-hidden');
+        // Toolbar click handlers
+        $('.iib-trigger', context).click(function() {
+          if ($('.page-iib').hasClass('iib-active')) {
+            $('.iib-trigger').removeClass('active');
+            $('.page-iib', context).removeClass('iib-active');
           }
           else {
-            $(this).addClass('active');
-            $('.page-iib', context).removeClass('iib-hidden');
+            $('.iib-trigger').addClass('active');
+            $('.page-iib', context).addClass('iib-active');
           }
-
           return false;
         });
+        // Force IIB to be active when displaying an error.
+        if ($('.page-iib .error')[0]) {
+          $('.iib-trigger').addClass('active');
+          $('.page-iib', context).addClass('iib-active');
+        }
+        // Close IIB if other navbar items are in use.
+        $('.icon:not(.iib-trigger)', context).click(function() {
+          $('.iib-trigger').removeClass('active');
+          $('.page-iib', context).removeClass('iib-active');
+        });
+        // IIB check for active SPS.
+        if ($('.page-iib #edit-cancel')[0]) {
+          $('#iib-navbar-tab').addClass('sps-active');
+        }
+        else {
+          $('#iib-navbar-tab').removeClass('sps-active');
+        }
       }
     };
 })(jQuery);
